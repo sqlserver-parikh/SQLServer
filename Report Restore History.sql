6c441d7a3bd13c7d77699ef4095618fa3a3d8c36
nimitparikh <27146137+nimitparikh@users.noreply.github.com>
2019-08-29 11:40:37 -0400

    Update Report Restore History.sql

--- Report Restore History.sql
+++ Report Restore History.sql
@@ -3,14 +3,16 @@ SELECT @@SERVERNAME CurrentServerName,
        DBRestored = destination_database_name,
        RestoreDate = restore_date,
        SourceDB = b.database_name,
+	RestoredDBBackupFileLocation = bmf.physical_device_name,
        SourceFile = physical_name,
        BackupDate = backup_start_date,
        SourceServer = server_name,
        CONVERT(DECIMAL(18, 2), b.backup_size / 1024.0 / 1024.0) BackupSize,
        h.user_name RestoredBy
-FROM RestoreHistory h
-     INNER JOIN BackupSet b ON h.backup_set_id = b.backup_set_id
-     INNER JOIN BackupFile f ON f.backup_set_id = b.backup_set_id
+FROM msdb..restorehistory h
+     INNER JOIN msdb..backupset b ON h.backup_set_id = b.backup_set_id
+     INNER JOIN msdb..backupfile f ON f.backup_set_id = b.backup_set_id
+     INNER JOIN MSDB..backupmediafamily bmf ON bmf.media_set_id = b.media_set_id
 WHERE 1 = 1
       --AND restore_date > DATEADD(DD, -30, GETDATE())
       --AND destination_database_name LIKE 'DBName'
