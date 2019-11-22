02bde359c3e52ed0d1e33442a815e7c991b73f70
nimitparikh <27146137+nimitparikh@users.noreply.github.com>
2019-11-22 13:33:34 -0500

    Update Report Security Database Roles.sql

--- Report Security Database Roles.sql
+++ Report Security Database Roles.sql
@@ -1,30 +1,30 @@
 USE master;
 GO
 IF OBJECT_ID(N'vwDBList') IS NOT NULL
-    DROP VIEW [dbo].[vwDBList];
+  DROP VIEW [dbo].[vwDBList];
 GO
 CREATE VIEW [dbo].[vwDBList]
 AS
-     SELECT sd.[name] AS 'DBName',
-            SUBSTRING(SUSER_SNAME(sd.[owner_sid]), 1, 24) AS 'Owner'
-     FROM master.sys.databases sd
-     WHERE HAS_DBACCESS(sd.[name]) = 1
-           AND sd.[is_read_only] = 0
-           AND sd.[state_desc] = 'ONLINE'
-           AND sd.[user_access_desc] = 'MULTI_USER'
-           AND sd.[is_in_standby] = 0;
+SELECT
+  sd.[name] AS 'DBName',
+  SUBSTRING(SUSER_SNAME(sd.[owner_sid]), 1, 24) AS 'Owner'
+FROM master.sys.databases sd
+WHERE HAS_DBACCESS(sd.[name]) = 1
+AND sd.[is_read_only] = 0
+AND sd.[state_desc] = 'ONLINE'
+AND sd.[user_access_desc] = 'MULTI_USER'
+AND sd.[is_in_standby] = 0;
 GO
-CREATE TABLE #temprole
-(DBName     VARCHAR(128),
- UserName   VARCHAR(128),
- LoginSID   VARBINARY(128),
- RoleName   VARCHAR(2000),
- CreateDate DATETIME,
- UpdateDate DATETIME
+CREATE TABLE #temprole (
+  DBName varchar(128),
+  UserName varchar(128),
+  LoginSID varbinary(128),
+  RoleName varchar(2000),
+  CreateDate datetime,
+  UpdateDate datetime
 );
 INSERT INTO #temprole
-EXEC sp_MSforeachdb
-'IF EXISTS (select TOP (1) 1 from sys.databases sd inner join dbo.vwDBList vw on sd.name = vw.DBName
+EXEC sp_MSforeachdb 'IF EXISTS (select TOP (1) 1 from sys.databases sd inner join dbo.vwDBList vw on sd.name = vw.DBName
 where sd.name = "?" ) BEGIN with cte
 as
 (
@@ -42,33 +42,100 @@ FOR XML PATH ('''')
 ),3,8000),createdate, updatedate FROM cte a
 end
 ';
-DECLARE @SQL VARCHAR(MAX);
+DECLARE @SQL varchar(max);
 SET @SQL = '';
-SELECT @SQL = @SQL+'SELECT '+QUOTENAME([name], '''')+' COLLATE Latin1_General_CI_AS AS [DBName], u.[name] COLLATE Latin1_General_CI_AS '+'FROM '+QUOTENAME([name])+'.dbo.sysusers u '+'WHERE u.issqluser = 1 '+'AND (u.sid is not null AND u.sid <> 0x0) '+'AND NOT EXISTS (SELECT 1 FROM master.dbo.syslogins l WHERE l.sid = u.sid) '+'UNION '
+SELECT
+  @SQL = @SQL + 'SELECT ' + QUOTENAME([name], '''') + ' COLLATE Latin1_General_CI_AS AS [DBName], u.[name] COLLATE Latin1_General_CI_AS ' + 'FROM ' + QUOTENAME([name]) + '.dbo.sysusers u ' + 'WHERE u.issqluser = 1 ' + 'AND (u.sid is not null AND u.sid <> 0x0) ' + 'AND NOT EXISTS (SELECT 1 FROM master.dbo.syslogins l WHERE l.sid = u.sid) ' + 'UNION '
 FROM master.dbo.sysdatabases sd
-     INNER JOIN master..vwDBList vw ON sd.name = vw.dbname;
+INNER JOIN master..vwDBList vw
+  ON sd.name = vw.dbname;
 SET @SQL = LEFT(@SQL, LEN(@SQL) - 5);
-CREATE TABLE #DBUsers
-(DBName   VARCHAR(255),
- Username VARCHAR(255)
+CREATE TABLE #DBUsers (
+  DBName varchar(255),
+  Username varchar(255)
 );
-INSERT INTO #DBUsers
-(DBName,
- Username
-)
+INSERT INTO #DBUsers (DBName,
+Username)
 EXEC (@SQL);
-SELECT @@SERVERNAME ServerName,
-       t.*,
-       d.Username,
-       sp.type_desc
+SELECT
+  @@SERVERNAME ServerName,
+  t.*,
+  sp.type_desc INTO #tmpDatabaseRoles
 FROM #temprole t
-     LEFT JOIN #DBUsers d ON t.DBName = d.DBName
-                             AND t.UserName = d.Username
-     INNER JOIN sys.server_principals sp ON sp.sid = t.LoginSID
+LEFT JOIN #DBUsers d
+  ON t.DBName = d.DBName
+  AND t.UserName = d.Username
+INNER JOIN sys.server_principals sp
+  ON sp.sid = t.LoginSID
+
 WHERE t.UserName NOT LIKE 'dbo'
-      AND t.UserName NOT LIKE '##%';
+AND t.UserName NOT LIKE '##%';
+BEGIN TRY
+  DECLARE @lname varchar(256)
+  CREATE TABLE #tmpGroupDetail (
+    AccountName varchar(256),
+    Type varchar(128),
+    Privilege varchar(128),
+    Mapped_Login_Name varchar(256),
+    Permission_Path varchar(256)
+  )
+
+  DECLARE Roles CURSOR FOR
+  SELECT
+    UserName
+  FROM #tmpDatabaseRoles
+  WHERE type_desc = 'WINDOWS_GROUP'
+  OPEN Roles;
+  FETCH NEXT FROM Roles INTO @lname;
+  WHILE @@FETCH_STATUS = 0
+  BEGIN
+    INSERT INTO #tmpGroupDetail
+    EXEC xp_logininfo @lname,
+                      members
+    FETCH NEXT FROM Roles INTO @lname;
+  END
+  SELECT
+    A.ServerName,
+    A.DBName,
+    A.UserName LoginName,
+    ISNULL(B.AccountName, 'Individual Login') GroupMembers,
+    A.RoleName DatabaseRoles,
+    A.CreateDate,
+    A.UpdateDate,
+    A.type_desc
+  FROM #tmpDatabaseRoles A
+  LEFT JOIN #tmpGroupDetail B
+    ON a.UserName = b.Permission_Path
+  ORDER BY type_desc
+  CLOSE Roles;
+  DEALLOCATE Roles;
+END TRY
+BEGIN CATCH
+--SELECT ERROR_NUMBER() AS ErrorNumber
+--     ,ERROR_SEVERITY() AS ErrorSeverity
+--     ,ERROR_STATE() AS ErrorState
+--     ,ERROR_PROCEDURE() AS ErrorProcedure
+--     ,ERROR_LINE() AS ErrorLine
+--     ,ERROR_MESSAGE() AS ErrorMessage;
+  SELECT
+    A.ServerName,
+    A.DBName,
+    A.UserName LoginName,
+    CASE
+      WHEN type_desc = 'WINDOWS_GROUP' THEN 'Not able to populate group members'
+      ELSE 'Individual Login'
+    END GroupMembers,
+
+    A.RoleName DatabaseRole,
+    A.CreateDate,
+    A.UpdateDate,
+    A.type_desc
+  FROM #tmpDatabaseRoles A
+  ORDER BY type_desc
+END CATCH
 GO
 DROP TABLE #temprole;
 DROP TABLE #DBUsers;
-DROP VIEW [vwDBList];
+DROP TABLE #tmpDatabaseRoles
+DROP TABLE #tmpGroupDetail
 GO
