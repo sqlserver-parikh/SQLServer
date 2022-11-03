407e473542bf49ce39d96896ffc5d8f1801b2038
nimitparikh <27146137+nimitparikh@users.noreply.github.com>
2022-11-03 15:50:34 -0400

    Update Shrink Files In Chunk.sql

--- Shrink Files In Chunk.sql
+++ Shrink Files In Chunk.sql
@@ -1,19 +1,25 @@
-DECLARE @filename VARCHAR(128);
+CREATE PROCEDURE #spShrink (@BatchSize int = 10000, @FreePct int =15, @MaxGBFree int = 50000, @filename varchar(128) = 'ALL')
+as 
+set nocount on;
 DECLARE @CurrentSize DECIMAL(20, 2);
 DECLARE @LoopCurrentSize DECIMAL(20, 2);
 DECLARE @ShrinkTo DECIMAL(20, 2);
-DECLARE @BatchSize INT= 10000;
 DECLARE @sql VARCHAR(2000);
+IF @filename = 'all' 
+begin
+set @filename = '%'
+end;
 WITH cte
      AS (SELECT name AS FileName, 
                 size / 128.0 AS CurrentSizeMB, 
                 CAST(FILEPROPERTY(name, 'SpaceUsed') AS INT) / 128.0 SpaceUsed
-         FROM sys.database_files),
+         FROM sys.database_files
+		 WHERE NAME LIKE @filename),
      CTE2
      AS (SELECT CASE
-                    WHEN((SpaceUsed * 0.15) > 50000)
+                    WHEN((SpaceUsed * (@FreePct / 100)) > @MaxGBFree)
                     THEN '50000'
-                    ELSE(SpaceUsed * 0.15)
+                    ELSE(SpaceUsed * (@FreePct / 100))
                 END FreeSpace, 
                 *
          FROM cte)
@@ -26,9 +32,9 @@ FOR SELECT FileName,
            CurrentSizeMB, 
            ShrinkTo
     FROM #temp
-    ORDER BY CurrentSizeMB ASC;
-SELECT *
-FROM #temp;
+    ORDER BY CurrentSizeMB desc;
+--SELECT *
+--FROM #temp;
 OPEN ShrinkDatabase;
 FETCH NEXT FROM ShrinkDatabase INTO @FileName, @CurrentSize, @ShrinkTo;
 WHILE @@FETCH_STATUS = 0
@@ -51,62 +57,7 @@ WHILE @@FETCH_STATUS = 0
 CLOSE ShrinkDatabase;
 DEALLOCATE ShrinkDatabase;
 DROP TABLE #temp;
-
-
-
-/*
-
---This is old code
---Below script can be used to Shrink data file in chunks of 500MB, it will either shrink data file to 30% or keep 30% free space, which ever is higher. Its not recommended to Shrink data files but we all need to do it as we fight for space :)
---Its not recommended to Shrink data files but I still do it :-) https://www.sqlskills.com/blogs/paul/why-you-should-not-shrink-your-data-files/ 
-
-USE master --Change it to appropriate database.
-
-SET NOCOUNT ON;
-GO
-DECLARE @filename VARCHAR(128);
-DECLARE @CurrentSize DECIMAL(20, 2);
-DECLARE @SpaceUsed DECIMAL(20, 2);
-DECLARE @ShrinkByMB int = 1500
-DECLARE ShrinkDatabase CURSOR
-FOR SELECT name AS FileName
-     , size / 128.0 AS CurrentSizeMB
-     , CAST(FILEPROPERTY(name, 'SpaceUsed') AS INT)/128.0 SpaceUsed
-    FROM sys.database_files
-    WHERE [type_desc] = 'ROWS';
-OPEN ShrinkDatabase;
-FETCH NEXT FROM ShrinkDatabase INTO @FileName, @CurrentSize, @SpaceUsed;
-WHILE @@FETCH_STATUS = 0
-    BEGIN
-    --SELECT @filename
-    -- , @CurrentSize
-    -- , @SpaceUsed;
-    DECLARE @target INT, @target2 INT;
-
---Please modify below to from 0.30 to different value keep more/less free space.
-
-    SET @target = @CurrentSize - (@CurrentSize * 0.30);
-    SET @target2 = @SpaceUsed + (@SpaceUsed * 0.30);
-    --SELECT @target LESS
-    -- , @target2 MORE;
-    IF(@target2 > @target)
-    BEGIN
-    SET @target = @target2;
-    END;
-    PRINT '--'+CONVERT(VARCHAR(128), @TARGET);
-    DECLARE @cycle INT;
-    DECLARE @sql VARCHAR(2000);
-    SET NOCOUNT ON;
-    SET @cycle = @CurrentSize;
-    WHILE 1 = 1
-    BEGIN
-    SET @cycle = @cycle - @ShrinkByMB;
-    SET @sql = 'DBCC SHRINKFILE('+quotename(@filename)+', '+CAST(@cycle AS VARCHAR)+')'+CHAR(10)+CHAR(13)+'GO'++CHAR(10)+CHAR(13);
-    PRINT(@sql);
-    IF @cycle < @target BREAK;
-    END;
-    FETCH NEXT FROM ShrinkDatabase INTO @FileName, @CurrentSize, @SpaceUsed;
-    END;
-CLOSE ShrinkDatabase;
-DEALLOCATE ShrinkDatabase;
-*/
+go
+exec #spShrink
+go
+drop procedure #spShrink
