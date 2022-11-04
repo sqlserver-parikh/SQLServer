296ecaca6147b4447090dcd483f402496082b1d7
nimitparikh <27146137+nimitparikh@users.noreply.github.com>
2022-11-04 13:11:31 -0400

    Update Copy Database Old New User Permission.sql

--- Copy Database Old New User Permission.sql
+++ Copy Database Old New User Permission.sql
@@ -1,71 +1,3 @@
---http://dba.stackexchange.com/questions/64567/how-to-clone-a-user-in-sql-server-2008-r2
---- To copy permissions of one user/role to another user/role.
-
-USE DBATasks; -- Use the database from which you want to extract the permissions
-GO
-SET NOCOUNT ON;
-DECLARE @OldUser SYSNAME, @NewUser SYSNAME;
-SET @OldUser = 'olduser'; --The user or role from which to copy the permissions from
-SET @NewUser = 'userNEW';  --The user or role to which to copy the permissions to
-
-SELECT 'USE'+SPACE(1)+QUOTENAME(DB_NAME()) AS '--Database Context';
-SELECT '--Cloning permissions from'+SPACE(1)+QUOTENAME(@OldUser)+SPACE(1)+'to'+SPACE(1)+QUOTENAME(@NewUser) AS '--Comment';
-SELECT 'EXEC sp_addrolemember @rolename ='+SPACE(1)+QUOTENAME(USER_NAME(rm.role_principal_id), '''')+', @membername ='+SPACE(1)+QUOTENAME(@NewUser, '''') AS '--Role Memberships'
-FROM sys.database_role_members AS rm
-WHERE USER_NAME(rm.member_principal_id) = @OldUser
-ORDER BY rm.role_principal_id ASC;
-SELECT CASE
-           WHEN perm.state <> 'W'
-           THEN perm.state_desc
-           ELSE 'GRANT'
-       END+SPACE(1)+perm.permission_name+SPACE(1)+'ON '+QUOTENAME(USER_NAME(obj.schema_id))+'.'+QUOTENAME(obj.name)+CASE
-                                                                                                                        WHEN cl.column_id IS NULL
-                                                                                                                        THEN SPACE(0)
-                                                                                                                        ELSE '('+QUOTENAME(cl.name)+')'
-                                                                                                                    END+SPACE(1)+'TO'+SPACE(1)+QUOTENAME(@NewUser) COLLATE database_default+CASE
-                                                                                                                                                                                                WHEN perm.state <> 'W'
-                                                                                                                                                                                                THEN SPACE(0)
-                                                                                                                                                                                                ELSE SPACE(1)+'WITH GRANT OPTION'
-                                                                                                                                                                                            END AS '--Object Level Permissions'
-FROM sys.database_permissions AS perm
-     INNER JOIN sys.objects AS obj ON perm.major_id = obj.[object_id]
-     INNER JOIN sys.database_principals AS usr ON perm.grantee_principal_id = usr.principal_id
-     LEFT JOIN sys.columns AS cl ON cl.column_id = perm.minor_id
-                                    AND cl.[object_id] = perm.major_id
-WHERE usr.name = @OldUser
-ORDER BY perm.permission_name ASC,
-         perm.state_desc ASC;
-SELECT CASE
-           WHEN perm.state <> 'W'
-           THEN perm.state_desc
-           ELSE 'GRANT'
-       END+SPACE(1)+perm.permission_name+SPACE(1)+SPACE(1)+'TO'+SPACE(1)+QUOTENAME(@NewUser) COLLATE database_default+CASE
-                                                                                                                          WHEN perm.state <> 'W'
-                                                                                                                          THEN SPACE(0)
-                                                                                                                          ELSE SPACE(1)+'WITH GRANT OPTION'
-                                                                                                                      END AS '--Database Level Permissions'
-FROM sys.database_permissions AS perm
-     INNER JOIN sys.database_principals AS usr ON perm.grantee_principal_id = usr.principal_id
-WHERE usr.name = @OldUser
-      AND perm.major_id = 0
-ORDER BY perm.permission_name ASC,
-         perm.state_desc ASC;
-         
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 USE [master]
 GO
@@ -85,14 +17,11 @@ CREATE OR ALTER PROCEDURE #sp_CloneRights
 AS
 BEGIN
     SET NOCOUNT ON
-
     CREATE TABLE #output (command nvarchar(4000))
-
     DECLARE @command nvarchar(4000),
             @sql nvarchar(max),
             @dbName nvarchar(128),
             @msg nvarchar(max)
-
     SELECT @sql = N'',
            @dbName = QUOTENAME(DB_NAME())
     SET NOCOUNT ON;
@@ -121,7 +50,6 @@ BEGIN
     WHERE SPs.name NOT LIKE '%##%' --and SPs.name not like '%nt %'
           AND SPs.name = @oldUser
           AND sp.type NOT IN ( 'COSQ', 'CO' );
-
     declare @permission varchar(1048)
     DECLARE SRoles CURSOR FOR
     SELECT ScriptToRun
@@ -143,10 +71,7 @@ BEGIN
     END
     CLOSE SRoles;
     DEALLOCATE SRoles;
-
     DROP TABLE #ServerLevelRoles;
-
-
     IF (NOT EXISTS
     (
         SELECT 1
@@ -160,7 +85,6 @@ BEGIN
         PRINT @MSG
         RETURN
     END
-
     INSERT INTO #output
     (
         command
@@ -170,17 +94,16 @@ BEGIN
     SELECT 'USE' + SPACE(1) + @dbName
     UNION ALL
     SELECT 'SET XACT_ABORT ON'
-
     IF (ISNULL(@NewLoginName, '') <> '')
     BEGIN
         SET @sql
             = N'USE ' + @dbName
               + N';
-        IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = @newUser)
-        BEGIN
             INSERT INTO #output(command)
             SELECT ''--Create user'' AS command
- 
+			            INSERT INTO #output(command)
+
+			SELECT ''IF NOT EXISTS (SELECT 1 FROM sys.database_principals where name = '' + QUOTENAME(@NewUser, '''''''')  + '' )''
             INSERT INTO #output(command)
             SELECT
                 ''CREATE USER '' + QUOTENAME(@NewUser) + '' FOR LOGIN '' + QUOTENAME(@NewLoginName) +
@@ -190,28 +113,24 @@ BEGIN
             FROM sys.database_principals dp
             INNER JOIN sys.server_principals sp ON dp.sid = sp.sid
             WHERE dp.name = @OldUser
-        END'
-
+        '
         EXEC sp_executesql @sql,
                            N'@OldUser sysname, @NewUser sysname, @NewLoginName sysname',
                            @OldUser = @OldUser,
                            @NewUser = @NewUser,
                            @NewLoginName = @NewLoginName
     END
-
     INSERT INTO #output
     (
         command
     )
     SELECT '--Cloning permissions from' + SPACE(1) + QUOTENAME(@OldUser) + SPACE(1) + 'to' + SPACE(1)
            + QUOTENAME(@NewUser)
-
     INSERT INTO #output
     (
         command
     )
     SELECT '--Role Memberships' AS command
-
     SET @sql
         = N'USE ' + @dbName
           + N';
@@ -221,18 +140,15 @@ BEGIN
     FROM    sys.database_role_members AS rm
     WHERE    USER_NAME(rm.member_principal_id) = @OldUser
     ORDER BY rm.role_principal_id ASC'
-
     EXEC sp_executesql @sql,
                        N'@OldUser sysname, @NewUser sysname',
                        @OldUser = @OldUser,
                        @NewUser = @NewUser
-
     INSERT INTO #output
     (
         command
     )
     SELECT '--Object Level Permissions'
-
     SET @sql
         = N'USE ' + @dbName
           + N';
@@ -254,18 +170,15 @@ BEGIN
         ON cl.column_id = perm.minor_id AND cl.[object_id] = perm.major_id
     WHERE    usr.name = @OldUser
     ORDER BY perm.permission_name ASC, perm.state_desc ASC'
-
     EXEC sp_executesql @sql,
                        N'@OldUser sysname, @NewUser sysname',
                        @OldUser = @OldUser,
                        @NewUser = @NewUser
-
     INSERT INTO #output
     (
         command
     )
     SELECT N'--Database Level Permissions'
-
     SET @sql
         = N'USE ' + @dbName
           + N';
@@ -281,44 +194,32 @@ BEGIN
     WHERE    usr.name = @OldUser
     AND    perm.major_id = 0
     ORDER BY perm.permission_name ASC, perm.state_desc ASC'
-
     EXEC sp_executesql @sql,
                        N'@OldUser sysname, @NewUser sysname',
                        @OldUser = @OldUser,
                        @NewUser = @NewUser
-
     DECLARE cr CURSOR FOR SELECT command FROM #output
-
     OPEN cr
-
     FETCH NEXT FROM cr
     INTO @command
-
     SET @sql = ''
-
     WHILE @@FETCH_STATUS = 0
     BEGIN
         IF (@printOnly IS NOT NULL)
             PRINT @command
-
         SET @sql = @sql + @command + CHAR(13) + CHAR(10)
         FETCH NEXT FROM cr
         INTO @command
     END
-
     CLOSE cr
     DEALLOCATE cr
-
     IF (@printOnly IS NULL OR @printOnly = 0) EXEC (@sql)
-
     DROP TABLE #output
 END
-
-
 GO
 GO
-declare @olduser varchar(128) = 'testing'
-declare @newuser varchar(128) = 'NEWUSER'
+declare @olduser varchar(128) = 'ssrs_usEr'
+declare @newuser varchar(128) = 'ssrs_user'
 declare @dbname varchar(128);
 Declare DBs cursor for
 SELECT sd.[name] AS 'DBName'
@@ -344,18 +245,3 @@ BEGIN
 END
 CLOSE dbs;
 DEALLOCATE dbs;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
