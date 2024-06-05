976ba531846c49fdb0ba68d077033b8288a0ef7f
sqlserver-parikh <27146137+sqlserver-parikh@users.noreply.github.com>
2024-06-04 21:23:11 -0400

    Update Report T-Log Backup Gap.sql

--- Report T-Log Backup Gap.sql
+++ Report T-Log Backup Gap.sql
@@ -1,8 +1,15 @@
 USE msdb;
-DECLARE @lookbackdays INT = 1;
-DECLARE @intervalhours INT = 2;
+DECLARE @lookbackdays INT = 15;
+DECLARE @intervalhours INT = 100;
+DECLARE @backuptype char(1) = 'D' -- D is for full, I for differential, L is for Log
 DECLARE @CurrentTime DATETIME = GETDATE();
 DECLARE @RoundedTime DATETIME;
+DECLARE @RECOVERYMODEL int;
+
+IF @backuptype = 'L'
+SET @RECOVERYMODEL = 3
+ELSE 
+SET @RECOVERYMODEL = 4
 
 -- Calculate the nearest 3-hour interval
 SET @RoundedTime = DATEADD(HOUR, ROUND(DATEDIFF(HOUR, '2000-01-01T00:00:00', @CurrentTime) / @intervalhours, 0) * @intervalhours, '2000-01-01T00:00:00');
@@ -22,8 +29,7 @@ DECLARE @IntervalStart DATETIME = @StartDate;
 -- Assuming you have a list of database names (replace with actual database names)
 DECLARE @DatabaseNames TABLE (DBName NVARCHAR(255));
 INSERT INTO @DatabaseNames (DBName)
-SELECT name FROM sys.databases d WHERE d.recovery_model_desc <> 'SIMPLE'
-    AND d.state_desc = 'ONLINE' AND d.database_id > 4;
+SELECT name FROM sys.databases d WHERE d.state_desc = 'ONLINE' AND database_id <> 2;
 
 WHILE @IntervalStart < @EndDate
 BEGIN
@@ -45,10 +51,10 @@ END
         ROW_NUMBER() OVER (PARTITION BY d.name ORDER BY bh.backup_finish_date DESC) AS rn
     FROM sys.databases d
     LEFT JOIN msdb.dbo.backupset bh ON d.name = bh.database_name
-        AND bh.type = 'L' -- Log backups
+        AND bh.type = @backuptype -- Log backups
+		AND database_id <> 2
         AND bh.backup_finish_date >= @StartDate
-        AND database_id > 4
-        AND d.recovery_model_desc <> 'SIMPLE'
+		AND D.recovery_model < @RECOVERYMODEL
 ), CTE2 AS (
     SELECT
         i.IntervalStart,
@@ -64,8 +70,7 @@ WHERE (bh.rn = 1 AND bh.backup_finish_date BETWEEN i.IntervalStart AND i.Interva
     SELECT cte2.*, d.recovery_model_desc, d.state_desc
     FROM CTE2
     LEFT JOIN sys.databases d ON cte2.database_name = d.name
-    WHERE d.database_id > 4
-    AND d.recovery_model_desc <> 'SIMPLE'
+    WHERE d.recovery_model < @RECOVERYMODEL
     AND d.state_desc = 'ONLINE'
 ), cte4 AS 
 (
@@ -74,7 +79,13 @@ WHERE (bh.rn = 1 AND bh.backup_finish_date BETWEEN i.IntervalStart AND i.Interva
     WHERE database_name IS NULL
     GROUP BY i.DBName
 )
-SELECT @@SERVERNAME ServerName, *, CONVERT (DECIMAL(10,2),(TotalFailed*100)/(convert(decimal (5,2),TotalIntervals))) PCTFailed FROM cte4
+SELECT @@SERVERNAME ServerName, *
+, case 
+	WHEN @backuptype = 'L' THEN 'LOG'
+	WHEN @backuptype = 'I' THEN 'DIFFERENTIAL'
+	WHEN @backuptype = 'D' THEN 'FULL'
+  END BackupReport 
+FROM cte4
 ORDER BY 1 DESC;
 GO
 
