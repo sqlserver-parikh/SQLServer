0b332234171f1cd4556d7a74f539bdbd8dc5f6b5
sqlserver-parikh <27146137+sqlserver-parikh@users.noreply.github.com>
2024-09-11 21:36:19 -0400

    Update Report Hardware Info - Memory CPU MAXDOP.sql
    FIXED ISSUE FOR AG without listner name.

--- Report Hardware Info - Memory CPU MAXDOP.sql
+++ Report Hardware Info - Memory CPU MAXDOP.sql
@@ -20,8 +20,7 @@ CREATE TABLE [dbo].[tbl_SQLInformation](
 	[IPAddress] [sql_variant] NULL,
 	[DomainNameList] [nvarchar](max) NULL,
 	[AllNodes] [nvarchar](max) NULL,
-	[Edition] [sql_variant] NULL,	
-	TraceFlagResult varchar(max) NULL,
+	[Edition] [sql_variant] NULL,
 	[ErrorLogLocation] [sql_variant] NULL,
 	[Data Files] [nvarchar](512) NULL,
 	[Log Files] [nvarchar](512) NULL,
@@ -74,18 +73,7 @@ SELECT @DomainNames = STUFF((
 DECLARE @TimeZone VARCHAR(50);
 DECLARE @IsDST BIT;
 DECLARE @UTCOffset VARCHAR(10);
-DECLARE @TraceResult NVARCHAR(MAX);
 
--- Temporary table to store trace flags
-CREATE TABLE #TraceFlags (TraceFlag INT, Status int, Global int, Session int);
-
--- Insert enabled trace flags into the temporary table
-INSERT INTO #TraceFlags
-EXEC ('DBCC TRACESTATUS(-1)');
-
--- Concatenate trace flags into a comma-separated string
-SELECT @TraceResult = STRING_AGG(CAST(TraceFlag AS NVARCHAR(MAX)), ', ') 
-FROM #TraceFlags;
 -- Read the time zone key name from the registry
 EXEC MASTER.dbo.xp_regread 'HKEY_LOCAL_MACHINE',
     'SYSTEM\CurrentControlSet\Control\TimeZoneInformation',
@@ -212,10 +200,10 @@ VALUES
                    availability_mode_desc
             INTO #aginfo
             FROM master.sys.availability_groups Groups
-                 INNER JOIN master.sys.availability_replicas Replicas ON Groups.group_id = Replicas.group_id
-                 INNER JOIN master.sys.dm_hadr_availability_group_states States ON Groups.group_id = States.group_id
-                 INNER JOIN sys.availability_databases_cluster ADC ON ADC.group_id = Groups.group_id
-                 INNER JOIN sys.availability_group_listeners agl ON agl.group_id = groups.group_id;
+                 LEFT JOIN master.sys.availability_replicas Replicas ON Groups.group_id = Replicas.group_id
+                 LEFT JOIN master.sys.dm_hadr_availability_group_states States ON Groups.group_id = States.group_id
+                 LEFT JOIN sys.availability_databases_cluster ADC ON ADC.group_id = Groups.group_id
+                 LEFT JOIN sys.availability_group_listeners agl ON agl.group_id = groups.group_id;
             IF @@ROWCOUNT = 0
                 BEGIN
                     SET @agname = 'No AlwaysON';
@@ -235,7 +223,7 @@ VALUES
                         FROM #aginfo b
                         WHERE IsPrimaryServer = 1
                               AND a.agname = b.agname
-                              AND a.dns_name = b.dns_name
+                              AND ISNULL(a.dns_name,'') = ISNULL(b.dns_name,'')
                     ), 
                                           @agserverlist = SUBSTRING(
                     (
@@ -243,7 +231,7 @@ VALUES
                                ', ' + b.replica_server_name + '(' + failover_mode_desc + ', ' + availability_mode_desc + ')'
                         FROM #aginfo b
                         WHERE a.agname = b.agname
-                              AND a.dns_name = b.dns_name FOR XML PATH('')
+                              AND ISNULL(a.dns_name,'') = ISNULL(b.dns_name,'') FOR XML PATH('')
                     ), 3, 8000), 
                                           @agdblist = SUBSTRING(
                     (
@@ -251,7 +239,7 @@ VALUES
                                ', ' + b.database_name
                         FROM #aginfo b
                         WHERE a.agname = b.agname
-                              AND a.dns_name = b.dns_name
+                              AND ISNULL(a.dns_name,'') = ISNULL(b.dns_name,'')
                         ORDER BY 1 FOR XML PATH('')
                     ), 3, 8000)
                     FROM #aginfo a;
@@ -471,7 +459,6 @@ EXEC master.dbo.xp_regread
                THEN 'Not Clustered'
            END AllNodes, 
            SERVERPROPERTY(N'Edition') Edition, 
-		   @TraceResult TraceFlagResult,
            SERVERPROPERTY('ErrorLogFileName') ErrorLogLocation, 
            @DefaultData AS 'Data Files', 
            @DefaultLog AS 'Log Files',
@@ -593,7 +580,3 @@ IF OBJECT_ID('tempdb..#WinNames') IS NOT NULL
     DROP TABLE #WinNames;
 IF OBJECT_ID('tempdb..#aginfo') IS NOT NULL
     DROP TABLE #aginfo
-IF OBJECT_ID('tempdb..#InstanceName') IS NOT NULL
-    DROP TABLE #InstanceName
-IF OBJECT_ID('tempdb..#TraceFlags') IS NOT NULL
-    DROP TABLE #TraceFlags
