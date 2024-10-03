32913d479bfed759c2f36f96c89a1279f3d65b41
sqlserver-parikh <27146137+sqlserver-parikh@users.noreply.github.com>
2024-10-03 14:44:54 -0400

    Update Collect sp_whoisactive data.sql

--- Collect sp_whoisactive data.sql
+++ Collect sp_whoisactive data.sql
@@ -1,22 +1,42 @@
 CREATE OR ALTER PROCEDURE usp_LogWhoIsActive
 (
     @destination_table VARCHAR(4000) = 'tblWhoIsActive',
-    @numberOfRuns INT = 2,
-    @delay INT = 9 -- Delay in seconds
+    @numberOfRuns INT = 1,
+    @delay INT = 60, -- Delay in seconds
+    @deleteAll INT = 10, -- General retention period in days
+    @tempdbThreshold INT = 400000, -- Tempdb size threshold in KB
+    @tempdbblockingRetentionHours INT = 24, -- Retention period for tempdb size threshold in hours
+    @blockingRetentionDays INT = 7, -- Retention period for blocking sessions in days
+	@LogData bit = 1
 )
 AS
 BEGIN
+
+	IF @LogData = 0
+	BEGIN
+	EXEC sp_WhoIsActive
+             @get_transaction_info = 1,
+             @get_plans = 1,
+             @get_full_inner_text = 1,
+             @get_outer_command = 1,
+             @find_block_leaders = 1
+	END
+	ELSE
+	BEGIN
     -- Validate the number of runs
     IF @numberOfRuns < 1 OR @numberOfRuns >= 100
     BEGIN
         RAISERROR('The number of runs must be greater than 0 and less than 100.', 16, 1);
         RETURN;
     END
-	IF @delay < 5 OR @delay >= 300
+
+    -- Validate the delay
+    IF @delay < 5 OR @delay >= 300
     BEGIN
-        RAISERROR('Delay must be between 5 and 300.', 16, 1);
+        RAISERROR('Delay must be between 5 and 300 seconds.', 16, 1);
         RETURN;
     END
+
     DECLARE @schema VARCHAR(4000);
     DECLARE @delayTime VARCHAR(8);
 
@@ -74,16 +94,16 @@ BEGIN
 
     -- Cleanup old data
     DELETE FROM tblWhoIsActive
-    WHERE collection_time < DATEADD(HOUR, -24, GETDATE())
-          AND CONVERT(INT, RTRIM(LTRIM(REPLACE(tempdb_current, ',', '')))) < 400000
+    WHERE collection_time < DATEADD(HOUR, -@tempdbblockingRetentionHours, GETDATE())
+          AND CONVERT(INT, RTRIM(LTRIM(REPLACE(tempdb_current, ',', '')))) < @tempdbThreshold
           AND (blocking_session_id IS NULL AND blocked_session_count = 0);
 
     DELETE FROM tblWhoIsActive
-    WHERE collection_time < DATEADD(DAY, -7, GETDATE())
-          AND CONVERT(INT, RTRIM(LTRIM(REPLACE(tempdb_current, ',', '')))) < 400000
-          AND (blocking_session_id IS NOT NULL AND blocked_session_count = 0);
-
+    WHERE collection_time < DATEADD(DAY, -@blockingRetentionDays, GETDATE())
+          AND CONVERT(INT, RTRIM(LTRIM(REPLACE(tempdb_current, ',', '')))) < @tempdbThreshold
+          
     DELETE FROM tblWhoIsActive
-    WHERE collection_time < DATEADD(DAY, -10, GETDATE());
+    WHERE collection_time < DATEADD(DAY, -@deleteAll, GETDATE());
+END
 END
 GO
