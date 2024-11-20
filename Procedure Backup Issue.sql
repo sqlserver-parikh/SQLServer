41d1c9b5ff07897c1d82079e1e2795d6d554dfdf
sqlserver-parikh <27146137+sqlserver-parikh@users.noreply.github.com>
2024-11-20 16:12:35 -0500

    Update Procedure Backup Issue.sql

--- Procedure Backup Issue.sql
+++ Procedure Backup Issue.sql
@@ -1,45 +1,70 @@
 --Schedule this procedure run under agent job. Most of monitoring tool should be able to alert as its logged in application event log.
 CREATE OR ALTER PROCEDURE usp_BackupIssue 
 (
-    @fullbackup int = 36, 
-    @logbackup int = 6, 
-    @lookbackdays int = 15
+    @fullbackup int = 168, 
+    @logbackup int = 12, 
+	@logSizeFullMB int = 124,
+    @lookbackdays int = 8
 )
 AS
 BEGIN
     SET NOCOUNT ON;
 
     DECLARE @DBMissingBackup NVARCHAR(MAX);
+	        ;WITH LogSizeCTE AS
+        (
+            SELECT db.[name] AS [Database], 
+            CONVERT(DECIMAL(18,2), ls.cntr_value/1024.0) AS [Log Size (MB)], 
+            CONVERT(DECIMAL(18,2), lu.cntr_value/1024.0) AS [Log Used (MB)],
+            ISNULL(CAST(CAST(lu.cntr_value AS FLOAT) / CAST(ls.cntr_value AS FLOAT) AS DECIMAL(18,2)) * 100, 0) AS [Log Used %]
+            FROM sys.databases AS db WITH (NOLOCK)
+            INNER JOIN sys.dm_os_performance_counters AS lu WITH (NOLOCK)
+            ON db.name = lu.instance_name
+            INNER JOIN sys.dm_os_performance_counters AS ls WITH (NOLOCK)
+            ON db.name = ls.instance_name
+            LEFT OUTER JOIN sys.dm_database_encryption_keys AS de WITH (NOLOCK)
+            ON db.database_id = de.database_id
+            WHERE lu.counter_name LIKE N'Log File(s) Used Size (KB)%' 
+            AND ls.counter_name LIKE N'Log File(s) Size (KB)%'
+            AND ls.cntr_value > 0
+        )
+        SELECT * 
+        INTO #LogSizeTemp
+        FROM LogSizeCTE;
 
     ;WITH CTE AS
     (
         SELECT 
-            ISNULL(d.[name], bs.[database_name]) AS [Database], 
+            ISNULL(d.[name], bs.[database_name]) AS DatabaseName, 
             d.recovery_model_desc AS [Recovery Model], 
             d.log_reuse_wait_desc AS [Log Reuse Wait Desc],
             MAX(CASE WHEN [type] = 'D' THEN bs.backup_finish_date ELSE NULL END) AS [Last Full Backup],
             MAX(CASE WHEN [type] = 'I' THEN bs.backup_finish_date ELSE NULL END) AS [Last Differential Backup],
-            MAX(CASE WHEN [type] = 'L' THEN bs.backup_finish_date ELSE NULL END) AS [Last Log Backup]
+            MAX(CASE WHEN [type] = 'L' THEN bs.backup_finish_date ELSE NULL END) AS [Last Log Backup],
+			LST.[Log Used (MB)] LogUsedMB, LST.[Log Used %] LogUsedPCT
         FROM sys.databases AS d WITH (NOLOCK)
-        LEFT OUTER JOIN msdb.dbo.backupset AS bs WITH (NOLOCK)
-            ON bs.[database_name] = d.[name] 
-            AND bs.backup_finish_date > DATEADD(DD, -@lookbackdays, GETDATE())
-        WHERE d.name <> N'tempdb'
+        LEFT JOIN msdb.dbo.backupset AS bs WITH (NOLOCK) ON bs.[database_name] = d.[name] 
+		LEFT JOIN #LogSizeTemp LST ON LST.[Database] = D.name
+        WHERE d.name <> N'tempdb' AND bs.backup_finish_date > DATEADD(DD, -@lookbackdays, GETDATE())
         GROUP BY ISNULL(d.[name], bs.[database_name]), d.recovery_model_desc, d.log_reuse_wait_desc, d.[name] 
+		, LST.[Log Used (MB)] , LST.[Log Used %] 
     )
-    SELECT * 
+    SELECT DatabaseName + '(Full Backup: ' + CONVERT(VARCHAR(18),ISNULL([Last Full Backup],''),120)
+				+ ', Log Backup: ' + CONVERT(VARCHAR(18),ISNULL([Last Log Backup],''),120)
+				+ ', LogUsedMB: ' + CONVERT(VARCHAR(20),LogUsedMB)
+				+ ', LogUsedPct: ' + CONVERT(VARCHAR(20),LogUsedPCT) + ')' as DBName, * 
     INTO #TempCTE
     FROM CTE
     WHERE 
         ([Last Full Backup] < DATEADD(HH, -@fullbackup, GETDATE()) 
         OR ([Last Log Backup] < DATEADD(HH, -@logbackup, GETDATE()) AND [Recovery Model] <> 'SIMPLE')) 
         OR [Last Full Backup] IS NULL 
-        OR ([Last Log Backup] IS NULL AND [Recovery Model] <> 'SIMPLE');
-
-    SELECT @DBMissingBackup = STRING_AGG([Database], ', ')
+        OR ([Last Log Backup] IS NULL AND [Recovery Model] <> 'SIMPLE')
+		OR LogUsedMB > @logSizeFullMB
+    SELECT @DBMissingBackup = STRING_AGG(DBName, ', ')
     FROM #TempCTE;
 
-    IF @@ROWCOUNT <> 0 
+    IF @DBMissingBackup IS NOT NULL
     BEGIN 
         DECLARE @message NVARCHAR(2048);
         SET @message = 'Either full backup is not completed in ' 
@@ -47,8 +72,9 @@ BEGIN
             + ' hours or transaction log backup is not taken in last ' 
             + CONVERT(VARCHAR(5), @logbackup) 
             + ' hours. Databases: ' + @DBMissingBackup;
-        RAISERROR(@message, 18, 1) WITH LOG;
+        RAISERROR(@message, 20, 1) WITH LOG;
     END
 
     DROP TABLE #TempCTE;
 END
+
