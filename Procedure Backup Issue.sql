6810f1d3237ff3145491e77752439191c0ef318a
sqlserver-parikh <27146137+sqlserver-parikh@users.noreply.github.com>
2024-11-22 09:42:02 -0500

    Update Procedure Backup Issue.sql

--- Procedure Backup Issue.sql
+++ Procedure Backup Issue.sql
@@ -4,12 +4,20 @@ CREATE OR ALTER PROCEDURE usp_BackupIssue
 (
     @fullbackup int = 168, --Alert if full backup not done in X hours
     @logbackup int = 24, --Alert if log backup not done in X hours
-	@logSizeFullMB int = 5120, --Alert if log size is above X MB
-    @lookbackdays int = 30
+	@logSizeFullMB int = 512000, --Alert if log size is above X MB
+    @lookbackdays int = 20
 )
 AS
 BEGIN
     SET NOCOUNT ON;
+	    SELECT 
+        database_id AS DBID,
+        SUM(CAST(size AS BIGINT) * 8 / 1024) AS DatabaseSizeMB
+    INTO #dbsize
+    FROM 
+        sys.master_files
+    GROUP BY 
+        database_id;
 	SELECT @@SERVERNAME ServerName,
        dmr.session_id SessionID,
 
@@ -72,7 +80,7 @@ BEGIN
                          ),
                   114
               ) TotalEstimatedTime_DDHHMMSS,
-       CONVERT(NUMERIC(6, 2), dmr.percent_complete) AS [Percent Complete],
+       CONVERT(NUMERIC(6, 2), dmr.percent_complete) AS [PctComplete],
        CONVERT(NUMERIC(10, 2), dmr.total_elapsed_time / 1000.0 / 60.0) AS [Elapsed Min], db_name(des.database_id) DBName, command
 	INTO #backupdetail
 FROM sys.dm_exec_requests dmr
@@ -106,29 +114,34 @@ WHERE command IN ( 'RESTORE DATABASE', 'BACKUP DATABASE' ,'BACKUP LOG');
     (
         SELECT 
             ISNULL(d.[name], bs.[database_name]) AS DatabaseName, 
-            d.recovery_model_desc AS [Recovery Model], 
+			FORMAT(B.DatabaseSizeMB, 'N0') DBSizeMB,
+            d.recovery_model_desc collate  SQL_Latin1_General_CP1_CI_AS AS [Recovery Model], 
             d.log_reuse_wait_desc AS [Log Reuse Wait Desc],
-            MAX(CASE WHEN [type] = 'D' THEN bs.backup_finish_date ELSE NULL END) AS [Last Full Backup],
-            MAX(CASE WHEN [type] = 'I' THEN bs.backup_finish_date ELSE NULL END) AS [Last Differential Backup],
-            MAX(CASE WHEN [type] = 'L' THEN bs.backup_finish_date ELSE NULL END) AS [Last Log Backup],
-			LST.[Log Used (MB)] LogUsedMB, LST.[Log Used %] LogUsedPCT, BD.ETA, BD.TimeElapsed_DDHHMMSS, BD.TimeRemaining_DDHHMMSS , BD.TotalEstimatedTime_DDHHMMSS,BD.command
+            MAX(CASE WHEN [type] = 'D' THEN bs.backup_finish_date ELSE '' END) AS [Last Full Backup],
+            MAX(CASE WHEN [type] = 'I' THEN bs.backup_finish_date ELSE '' END) AS [Last Differential Backup],
+            MAX(CASE WHEN [type] = 'L' THEN bs.backup_finish_date ELSE '' END) AS [Last Log Backup],
+			LST.[Log Used (MB)] LogUsedMB, LST.[Log Used %] LogUsedPCT, 
+			BD.PctComplete, BD.ETA, BD.TimeElapsed_DDHHMMSS, BD.TimeRemaining_DDHHMMSS, 
+			BD.TotalEstimatedTime_DDHHMMSS,BD.command
         FROM sys.databases AS d WITH (NOLOCK)
         LEFT JOIN msdb.dbo.backupset AS bs WITH (NOLOCK) ON bs.[database_name] = d.[name] 
 		LEFT JOIN #LogSizeTemp LST ON LST.[Database] = D.name
 		LEFT JOIN #backupdetail BD ON BD.DBName = D.name
-        WHERE d.name <> N'tempdb' AND bs.backup_finish_date > DATEADD(DD, -20, GETDATE())
+		LEFT JOIN #dbsize b ON D.database_id = B.DBID
+        WHERE d.name <> N'tempdb' AND bs.backup_finish_date > DATEADD(DD, -@lookbackdays, GETDATE())
 		AND D.is_read_only = 0 and source_database_id is null
-        GROUP BY ISNULL(d.[name], bs.[database_name]), d.recovery_model_desc, d.log_reuse_wait_desc, d.[name] 
-		, LST.[Log Used (MB)] , LST.[Log Used %] , BD.ETA, BD.TimeElapsed_DDHHMMSS, BD.TimeRemaining_DDHHMMSS , BD.TotalEstimatedTime_DDHHMMSS, BD.command
+        GROUP BY ISNULL(d.[name], bs.[database_name]), d.recovery_model_desc, d.log_reuse_wait_desc 
+		, LST.[Log Used (MB)] , LST.[Log Used %] , BD.ETA, BD.TimeElapsed_DDHHMMSS, BD.TimeRemaining_DDHHMMSS ,PctComplete, BD.TotalEstimatedTime_DDHHMMSS, BD.command, FORMAT(B.DatabaseSizeMB, 'N0')
     )
-    SELECT DatabaseName + '(Full Backup: ' + CONVERT(VARCHAR(18),ISNULL([Last Full Backup],''),120)
-				+ ', Log Backup: ' + CONVERT(VARCHAR(18),ISNULL([Last Log Backup],''),120)
+    SELECT DatabaseName + '(' + [Recovery Model] + ', DBSize: ' + DBSizeMB  + 'MB, Full Backup: ' + CONVERT(VARCHAR(20),ISNULL([Last Full Backup],''),120)
+				+ ', Log Backup: ' + CONVERT(VARCHAR(20),ISNULL([Last Log Backup],''),120)
 				+ ', LogUsedMB: ' + CONVERT(VARCHAR(20),LogUsedMB)
 				+ ', LogUsedPct: ' + CONVERT(VARCHAR(20),LogUsedPCT)
-				+ ', Command: ' + ISNULL(command ,'Not Running')
-				+ ', BackupETA: ' + CONVERT(VARCHAR(20),ISNULL(ETA,'Not Running') ) 
-				+ ', TimeElapsed: ' + CONVERT(VARCHAR(20),ISNULL(TimeElapsed_DDHHMMSS,'Not Running'))
-				+ ', TimeRemaining: ' + CONVERT(VARCHAR(20),ISNULL(TimeRemaining_DDHHMMSS,'Not Running'))
+				+ ', Command: ' + ISNULL(command ,'NA')
+				+ ', PctComplete: ' + ISNULL(CONVERT(varchar(20), PctComplete),'NA')
+				+ ', BackupETA: ' + CONVERT(VARCHAR(20),ISNULL(ETA,'NA') ) 
+				+ ', TimeElapsed: ' + CONVERT(VARCHAR(20),ISNULL(TimeElapsed_DDHHMMSS,'NA'))
+				+ ', TimeRemaining: ' + CONVERT(VARCHAR(20),ISNULL(TimeRemaining_DDHHMMSS,'NA'))
 				+ ')' as DBName, * 
     INTO #TempCTE
     FROM CTE
@@ -138,8 +151,10 @@ WHERE command IN ( 'RESTORE DATABASE', 'BACKUP DATABASE' ,'BACKUP LOG');
         OR [Last Full Backup] IS NULL 
         OR ([Last Log Backup] IS NULL AND [Recovery Model] <> 'SIMPLE')
 		OR LogUsedMB > @logSizeFullMB
+	
 	select * from #TempCTE
-    SELECT @DBMissingBackup = STRING_AGG(DBName, ', ')
+    
+	SELECT @DBMissingBackup = STRING_AGG(DBName, ', ')
     FROM #TempCTE;
 
     IF @DBMissingBackup IS NOT NULL
