a5b153b13596dfa5e242bf358c1bea5819cc966d
sqlserver-parikh <27146137+sqlserver-parikh@users.noreply.github.com>
2025-08-11 13:26:54 -0400

    Update Report Security Database Roles.sql

--- Report Security Database Roles.sql
+++ Report Security Database Roles.sql
@@ -2,175 +2,170 @@ USE tempdb;
 GO
 
 CREATE OR ALTER PROCEDURE dbo.sp_GetDatabaseRoles
-    @DatabaseName NVARCHAR(128) = NULL,              -- Optional: Filter by specific database
-    @LoginName NVARCHAR(128) = NULL,                -- Optional: Filter by specific login
-    @RoleName NVARCHAR(128) = NULL,                 -- Optional: Filter by specific role
-    @IncludeSystemDatabases BIT = 0,                -- Optional: Include system databases
-    @ExcludeWindowsGroups BIT = 0,                  -- Optional: Exclude Windows Groups
-    @ShowOnlyOrphanedUsers BIT = 0,                 -- Optional: Show only orphaned users
-    @Debug BIT = 0                                  -- Optional: Show debug information
+    @DatabaseName NVARCHAR(128) = NULL,             -- Optional: Filter by specific database
+    @LoginName NVARCHAR(128) = NULL,               -- Optional: Filter by specific login
+    @RoleName NVARCHAR(128) = NULL,                -- Optional: Filter by specific role
+    @IncludeSystemDatabases BIT = 0,               -- Optional: Include system databases
+    @ExcludeWindowsGroups BIT = 0,                 -- Optional: Exclude Windows Groups
+    @ShowOnlyOrphanedUsers BIT = 0,                -- Optional: Show only orphaned users
+    @Debug BIT = 0                                 -- Optional: Show debug information
 AS
 BEGIN
     SET NOCOUNT ON;
+
+    -- Create all temp tables at the start to ensure they exist for the CATCH block.
+    CREATE TABLE #temprole (
+        DBName sysname,
+        UserName sysname,
+        LoginSID VARBINARY(85),
+        RoleName VARCHAR(2000),
+        CreateDate DATETIME,
+        UpdateDate DATETIME,
+        UserType CHAR(1)
+    );
+
+    CREATE TABLE #DBUsers (
+        DBName sysname,
+        Username sysname
+    );
     
+    CREATE TABLE #tmpDatabaseRoles (
+        ServerName sysname,
+        DBName sysname,
+        UserName sysname,
+        LoginSID VARBINARY(85),
+        RoleName VARCHAR(2000),
+        CreateDate DATETIME,
+        UpdateDate DATETIME,
+        type_desc NVARCHAR(60)
+    );
+
+    CREATE TABLE #tmpGroupDetail (
+        AccountName sysname,
+        Type VARCHAR(128),
+        Privilege VARCHAR(128),
+        Mapped_Login_Name sysname,
+        Permission_Path sysname
+    );
+
+
     BEGIN TRY
-        -- Temporary tables
-        CREATE TABLE #temprole (
-            DBName varchar(128),
-            UserName varchar(128),
-            LoginSID varbinary(128),
-            RoleName varchar(2000),
-            CreateDate datetime,
-            UpdateDate datetime
-        );
+        -- Use a reliable cursor instead of the undocumented sp_MSforeachdb
+        DECLARE @db_name sysname;
+        DECLARE @SqlCmd NVARCHAR(MAX);
 
-        -- Dynamic SQL for database iteration
-        DECLARE @SqlCmd NVARCHAR(MAX) = N'
-        IF EXISTS (
-            SELECT TOP (1) 1 
-            FROM sys.databases sd 
-            WHERE sd.name = "?"
-            AND HAS_DBACCESS(sd.name) = 1
-            AND sd.is_read_only = 0
-            AND sd.state_desc = ''ONLINE''
-            AND sd.user_access_desc = ''MULTI_USER''
-            AND sd.is_in_standby = 0'
-            + CASE 
-                WHEN @IncludeSystemDatabases = 0 
-                THEN ' AND sd.name NOT IN (''master'', ''model'', ''msdb'', ''tempdb'')' 
-                ELSE '' 
-              END
-            + CASE 
-                WHEN @DatabaseName IS NOT NULL 
-                THEN ' AND sd.name = @DatabaseName' 
-                ELSE '' 
-              END + '
-        ) 
-        BEGIN 
-            WITH cte AS (
-                SELECT 
-                    "?" DBName,
-                    b.name AS UserName,
-                    b.sid,
-                    c.name AS RoleName,
-                    b.createdate,
-                    b.updatedate
-                FROM [?].dbo.sysmembers a
-                JOIN [?].dbo.sysusers b ON a.memberuid = b.uid 
-                JOIN [?].dbo.sysusers c ON a.groupuid = c.uid
-                WHERE 1=1 '
-                + CASE 
-                    WHEN @LoginName IS NOT NULL 
-                    THEN ' AND b.name LIKE @LoginName' 
-                    ELSE '' 
-                  END
-                + CASE 
-                    WHEN @RoleName IS NOT NULL 
-                    THEN ' AND c.name LIKE @RoleName' 
-                    ELSE '' 
-                  END + '
-            )
-            SELECT DISTINCT 
-                DBName,
-                UserName,
-                sid,
-                RoleName = SUBSTRING(
-                    (SELECT ('', '' + RoleName)
-                     FROM cte b
-                     WHERE a.UserName = b.UserName
-                     FOR XML PATH ('''')
-                    ), 3, 8000
-                ),
-                createdate,
-                updatedate 
-            FROM cte a
-        END';
+        DECLARE db_cursor CURSOR LOCAL FAST_FORWARD FOR 
+            SELECT name 
+            FROM sys.databases
+            WHERE state_desc = 'ONLINE'
+              AND HAS_DBACCESS(name) = 1
+              AND (@DatabaseName IS NULL OR name = @DatabaseName)
+              AND (@IncludeSystemDatabases = 1 OR database_id > 4);
 
-        IF @Debug = 1
-            PRINT @SqlCmd;
+        OPEN db_cursor;
+        FETCH NEXT FROM db_cursor INTO @db_name;
 
-        -- Execute for each database
-        INSERT INTO #temprole
-        EXEC sp_MSforeachdb @SqlCmd;
-
-        -- Handle orphaned users
-        CREATE TABLE #DBUsers (
-            DBName varchar(255),
-            Username varchar(255)
-        );
-
-        DECLARE @OrphanSql NVARCHAR(MAX) = N'';
-        
-        SELECT @OrphanSql = @OrphanSql + 
-            'SELECT ' + QUOTENAME([name], '''') + ' COLLATE Latin1_General_CI_AS AS [DBName], 
-                    u.[name] COLLATE Latin1_General_CI_AS ' +
-            'FROM ' + QUOTENAME([name]) + '.dbo.sysusers u ' +
-            'WHERE u.issqluser = 1 ' +
-            'AND (u.sid is not null AND u.sid <> 0x0) ' +
-            'AND NOT EXISTS (SELECT 1 FROM master.dbo.syslogins l WHERE l.sid = u.sid) '
-            + CASE 
-                WHEN @LoginName IS NOT NULL 
-                THEN ' AND u.name LIKE @LoginName' 
-                ELSE '' 
-              END + '
-            UNION ALL '
-        FROM master.dbo.sysdatabases sd
-        WHERE HAS_DBACCESS(sd.name) = 1
-        AND (@DatabaseName IS NULL OR sd.name = @DatabaseName)
-        AND (@IncludeSystemDatabases = 1 OR sd.name NOT IN ('master', 'model', 'msdb', 'tempdb'));
-
-        SET @OrphanSql = LEFT(@OrphanSql, LEN(@OrphanSql) - 9);
-
-        IF @Debug = 1
-            PRINT @OrphanSql;
-
-        IF @ShowOnlyOrphanedUsers = 1
+        WHILE @@FETCH_STATUS = 0
         BEGIN
-            INSERT INTO #DBUsers
-            EXEC sp_executesql @OrphanSql, 
-                N'@LoginName NVARCHAR(128)', 
-                @LoginName;
+            SET @SqlCmd = N'
+            USE ' + QUOTENAME(@db_name) + ';
+
+            WITH DBRoles AS (
+                SELECT 
+                    u.name AS UserName,
+                    u.sid AS LoginSID,
+                    r.name AS RoleName,
+                    u.create_date AS CreateDate,
+                    u.modify_date AS UpdateDate,
+                    u.type AS UserType
+                FROM sys.database_role_members rm
+                JOIN sys.database_principals r ON rm.role_principal_id = r.principal_id
+                JOIN sys.database_principals u ON rm.member_principal_id = u.principal_id
+                WHERE u.type IN (''S'', ''U'', ''G'') -- SQL_USER, WINDOWS_USER, WINDOWS_GROUP
+                  AND u.principal_id > 4 -- Exclude system users
+                  ' + CASE WHEN @LoginName IS NOT NULL THEN ' AND u.name = @pLoginName' ELSE '' END + '
+                  ' + CASE WHEN @RoleName  IS NOT NULL THEN ' AND r.name = @pRoleName'  ELSE '' END + '
+            )
+            INSERT INTO #temprole (DBName, UserName, LoginSID, RoleName, CreateDate, UpdateDate, UserType)
+            SELECT
+                DB_NAME() AS DBName,
+                a.UserName,
+                a.LoginSID,
+                STUFF((SELECT '', '' + b.RoleName
+                       FROM DBRoles b
+                       WHERE a.UserName = b.UserName
+                       FOR XML PATH('''')), 1, 2, '''') AS RoleName,
+                a.CreateDate,
+                a.UpdateDate,
+                a.UserType
+            FROM DBRoles a
+            GROUP BY a.UserName, a.LoginSID, a.CreateDate, a.UpdateDate, a.UserType;';
+            
+            IF @Debug = 1 PRINT @SqlCmd;
+
+            EXEC sp_executesql @SqlCmd, 
+                               N'@pLoginName NVARCHAR(128), @pRoleName NVARCHAR(128)', 
+                               @pLoginName = @LoginName, @pRoleName = @RoleName;
+
+            FETCH NEXT FROM db_cursor INTO @db_name;
         END
 
-        -- Final results table
-        CREATE TABLE #tmpDatabaseRoles (
-            ServerName nvarchar(128),
-            DBName varchar(128),
-            UserName varchar(128),
-            LoginSID varbinary(128),
-            RoleName varchar(2000),
-            CreateDate datetime,
-            UpdateDate datetime,
-            type_desc nvarchar(60)
-        );
+        CLOSE db_cursor;
+        DEALLOCATE db_cursor;
+
+
+        -- Handle orphaned users
+        IF @ShowOnlyOrphanedUsers = 1
+        BEGIN
+            DECLARE @OrphanSql NVARCHAR(MAX) = N'';
+            
+            SELECT @OrphanSql = @OrphanSql + 
+                'SELECT ' + QUOTENAME([name], '''') + ' AS [DBName], 
+                        u.[name] ' +
+                'FROM ' + QUOTENAME([name]) + '.sys.database_principals u ' +
+                'WHERE u.type = ''S'' ' + -- SQL_USER
+                'AND u.sid IS NOT NULL AND u.sid <> 0x01 ' + -- sid is valid
+                'AND NOT EXISTS (SELECT 1 FROM sys.server_principals l WHERE l.sid = u.sid) '
+                + CASE WHEN @LoginName IS NOT NULL THEN ' AND u.name = @LoginName' ELSE '' END + '
+                UNION ALL '
+            FROM sys.databases sd
+            WHERE HAS_DBACCESS(sd.name) = 1
+                AND sd.state_desc = 'ONLINE'
+                AND (@DatabaseName IS NULL OR sd.name = @DatabaseName)
+                AND (@IncludeSystemDatabases = 1 OR sd.database_id > 4);
+
+            IF LEN(@OrphanSql) > 0
+            BEGIN
+                SET @OrphanSql = LEFT(@OrphanSql, LEN(@OrphanSql) - 10); -- Remove last 'UNION ALL '
+                IF @Debug = 1 PRINT @OrphanSql;
+                INSERT INTO #DBUsers (DBName, Username)
+                EXEC sp_executesql @OrphanSql, N'@LoginName NVARCHAR(128)', @LoginName;
+            END
+        END
 
         -- Populate final results
-        INSERT INTO #tmpDatabaseRoles
+        INSERT INTO #tmpDatabaseRoles (ServerName, DBName, UserName, LoginSID, RoleName, CreateDate, UpdateDate, type_desc)
         SELECT 
             @@SERVERNAME,
-            t.*,
+            t.DBName,
+            t.UserName,
+            t.LoginSID,
+            t.RoleName,
+            t.CreateDate,
+            t.UpdateDate,
             sp.type_desc
         FROM #temprole t
-        LEFT JOIN #DBUsers d ON t.DBName = d.DBName AND t.UserName = d.Username
         INNER JOIN sys.server_principals sp ON sp.sid = t.LoginSID
-        WHERE t.UserName NOT LIKE 'dbo'
-        AND t.UserName NOT LIKE '##%'
-        AND (@ExcludeWindowsGroups = 0 OR sp.type_desc != 'WINDOWS_GROUP');
+        WHERE 1 = 1
+            AND (@ExcludeWindowsGroups = 0 OR sp.type <> 'G')
+            AND (@ShowOnlyOrphanedUsers = 0 OR EXISTS (SELECT 1 FROM #DBUsers o WHERE o.DBName = t.DBName AND o.Username = t.UserName));
 
-        -- Handle Windows Groups if needed
+        -- Final output logic
         IF @ExcludeWindowsGroups = 0
         BEGIN
-            DECLARE @lname varchar(256);
+            DECLARE @lname sysname;
             
-            CREATE TABLE #tmpGroupDetail (
-                AccountName varchar(256),
-                Type varchar(128),
-                Privilege varchar(128),
-                Mapped_Login_Name varchar(256),
-                Permission_Path varchar(256)
-            );
-
-            DECLARE Roles CURSOR FOR
+            DECLARE Roles CURSOR LOCAL FAST_FORWARD FOR
                 SELECT UserName
                 FROM #tmpDatabaseRoles
                 WHERE type_desc = 'WINDOWS_GROUP';
@@ -180,12 +175,19 @@ BEGIN
 
             WHILE @@FETCH_STATUS = 0
             BEGIN
-                INSERT INTO #tmpGroupDetail
-                EXEC xp_logininfo @lname, 'members';
+                BEGIN TRY
+                    INSERT INTO #tmpGroupDetail (AccountName, Type, Privilege, Mapped_Login_Name, Permission_Path)
+                    EXEC xp_logininfo @lname, 'members';
+                END TRY
+                BEGIN CATCH
+                    PRINT 'Could not expand group: ' + @lname + '. Error: ' + ERROR_MESSAGE();
+                END CATCH
                 FETCH NEXT FROM Roles INTO @lname;
             END
 
-            -- Final result set with group details
+            CLOSE Roles;
+            DEALLOCATE Roles;
+
             SELECT
                 A.ServerName,
                 A.DBName,
@@ -196,15 +198,11 @@ BEGIN
                 A.UpdateDate,
                 A.type_desc
             FROM #tmpDatabaseRoles A
-            LEFT JOIN #tmpGroupDetail B ON a.UserName = b.Permission_Path
+            LEFT JOIN #tmpGroupDetail B ON A.UserName = B.Permission_Path
             ORDER BY A.type_desc, A.DBName, A.UserName;
-
-            CLOSE Roles;
-            DEALLOCATE Roles;
         END
         ELSE
         BEGIN
-            -- Final result set without group details
             SELECT
                 ServerName,
                 DBName,
@@ -213,14 +211,25 @@ BEGIN
                 RoleName AS DatabaseRoles,
                 CreateDate,
                 UpdateDate,
-                type_desc,
-				GETUTCDATE() ReportRunUTC
+                type_desc
             FROM #tmpDatabaseRoles
             ORDER BY type_desc, DBName, UserName;
         END
 
     END TRY
     BEGIN CATCH
+        IF CURSOR_STATUS('local', 'db_cursor') >= 0
+        BEGIN
+            CLOSE db_cursor;
+            DEALLOCATE db_cursor;
+        END
+        
+        IF CURSOR_STATUS('local', 'Roles') >= 0
+        BEGIN
+            CLOSE Roles;
+            DEALLOCATE Roles;
+        END
+
         SELECT
             ERROR_NUMBER() AS ErrorNumber,
             ERROR_SEVERITY() AS ErrorSeverity,
@@ -228,47 +237,11 @@ BEGIN
             ERROR_PROCEDURE() AS ErrorProcedure,
             ERROR_LINE() AS ErrorLine,
             ERROR_MESSAGE() AS ErrorMessage;
-
-        -- Simplified result set in case of error
-        SELECT
-            A.ServerName,
-            A.DBName,
-            A.UserName AS LoginName,
-            CASE
-                WHEN type_desc = 'WINDOWS_GROUP' THEN 'Not able to populate group members'
-                ELSE 'Individual Login'
-            END AS GroupMembers,
-            A.RoleName AS DatabaseRole,
-            A.CreateDate,
-            A.UpdateDate,
-            A.type_desc
-        FROM #tmpDatabaseRoles A
-        ORDER BY type_desc;
     END CATCH;
 
-    -- Cleanup
     DROP TABLE IF EXISTS #temprole;
     DROP TABLE IF EXISTS #DBUsers;
     DROP TABLE IF EXISTS #tmpDatabaseRoles;
     DROP TABLE IF EXISTS #tmpGroupDetail;
 END;
 GO
-
--- Example usage:
--- Get all database roles
- EXEC dbo.sp_GetDatabaseRoles;
-
--- Get roles for specific database
--- EXEC dbo.sp_GetDatabaseRoles @DatabaseName = 'YourDatabase';
-
--- Get roles for specific login
--- EXEC dbo.sp_GetDatabaseRoles @LoginName = 'YourLogin';
-
--- Get roles excluding Windows Groups
--- EXEC dbo.sp_GetDatabaseRoles @ExcludeWindowsGroups = 1;
-
--- Get only orphaned users
--- EXEC dbo.sp_GetDatabaseRoles @ShowOnlyOrphanedUsers = 1;
-
--- Get roles with debug information
--- EXEC dbo.sp_GetDatabaseRoles @Debug = 1;
