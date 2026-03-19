634f7858c12f461ee181e09625fd0196b49be797
sqlserver-parikh <27146137+sqlserver-parikh@users.noreply.github.com>
2026-03-19 15:24:55 -0400

    Refactor SQL script for fixing orphaned users

--- Fix Orphan Users.sql
+++ Fix Orphan Users.sql
@@ -1,124 +1,137 @@
-USE TEMPDB
+USE [tempdb]
 GO
 
-CREATE OR ALTER PROCEDURE usp_FixOrphanUsers
-    @DatabaseName NVARCHAR(255) = '',
-    @PrintOnly BIT = 0
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[usp_FixOrphanUsers]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[usp_FixOrphanUsers] AS' 
+END
+GO
+
+ALTER PROCEDURE [dbo].[usp_FixOrphanUsers]
+    @DatabaseName NVARCHAR(255) = NULL,
+    @Print        BIT = 0,
+    @Execute      BIT = 1
 AS
+/*********************************************************************************
+    Ola Hallengren Style Documentation
+    
+    Description: Identifies and fixes orphaned users.
+                 - If a matching Login exists, it re-links the User.
+                 - If no matching Login exists, it transfers schema ownership 
+                   to dbo and drops the User.
+
+    Parameters:
+    @DatabaseName: Specific database to process. If NULL or empty, all databases.
+    @Print:        Print the generated T-SQL commands.
+    @Execute:      Execute the generated T-SQL commands.
+*********************************************************************************/
 BEGIN
     SET NOCOUNT ON;
 
-    DECLARE @userid NVARCHAR(128);
-    DECLARE @dbname NVARCHAR(128);
-    DECLARE @sql NVARCHAR(MAX);
+    DECLARE @CurrentDBName NVARCHAR(128);
+    DECLARE @UserName      NVARCHAR(128);
+    DECLARE @DynamicSQL    NVARCHAR(MAX);
+    DECLARE @ErrorMessage  NVARCHAR(MAX);
 
-    -- Table to collect orphaned users
-    IF OBJECT_ID('tempdb..#OrphanUsers') IS NOT NULL DROP TABLE #OrphanUsers;
+    IF OBJECT_ID('tempdb..#OrphanUsers') IS NOT NULL 
+        DROP TABLE #OrphanUsers;
+
     CREATE TABLE #OrphanUsers (
         DBName   NVARCHAR(128),
         UserName NVARCHAR(128)
     );
 
-    -- Cursor for databases
+    -- Database discovery
     DECLARE db_cursor CURSOR FOR
-        SELECT name
-        FROM sys.databases
-        WHERE
-            state_desc = 'ONLINE'
-            AND is_read_only = 0
-            AND database_id > 4 -- Exclude system DBs
-            AND (@DatabaseName = '' OR name = @DatabaseName);
+    SELECT name
+    FROM sys.databases
+    WHERE state_desc = 'ONLINE'
+      AND is_read_only = 0
+      AND database_id > 4 
+      AND (name = @DatabaseName OR @DatabaseName IS NULL OR @DatabaseName = '');
 
     OPEN db_cursor;
-    FETCH NEXT FROM db_cursor INTO @dbname;
+    FETCH NEXT FROM db_cursor INTO @CurrentDBName;
 
     WHILE @@FETCH_STATUS = 0
     BEGIN
-        SET @sql = N'
+        SET @DynamicSQL = N'
             INSERT INTO #OrphanUsers (DBName, UserName)
             SELECT
-                N''' + REPLACE(@dbname, '''', '''''') + N''' AS DBName,
+                N''' + REPLACE(@CurrentDBName, '''', '''''') + N''' AS DBName,
                 name AS UserName
-            FROM ' + QUOTENAME(@dbname) + N'.sys.database_principals
+            FROM ' + QUOTENAME(@CurrentDBName) + N'.sys.database_principals
             WHERE type IN (''S'', ''U'')
-              AND authentication_type IN (1, 2) -- SQL or Windows users
+              AND authentication_type IN (1, 2)
               AND sid IS NOT NULL AND sid <> 0x0
               AND SUSER_SNAME(sid) IS NULL
-              AND name NOT IN (''dbo'', ''guest'', ''INFORMATION_SCHEMA'', ''sys'')
-        ';
-        EXEC sp_executesql @sql;
+              AND name NOT IN (''dbo'', ''guest'', ''INFORMATION_SCHEMA'', ''sys'')';
         
-        FETCH NEXT FROM db_cursor INTO @dbname;
+        EXEC sp_executesql @DynamicSQL;
+        FETCH NEXT FROM db_cursor INTO @CurrentDBName;
     END
 
     CLOSE db_cursor;
     DEALLOCATE db_cursor;
 
-    -- Cursor for orphaned users
+    -- Processing Orphans
     DECLARE user_cursor CURSOR FOR
-        SELECT UserName, DBName FROM #OrphanUsers;
+    SELECT UserName, DBName FROM #OrphanUsers;
 
     OPEN user_cursor;
-    FETCH NEXT FROM user_cursor INTO @userid, @dbname;
+    FETCH NEXT FROM user_cursor INTO @UserName, @CurrentDBName;
 
     WHILE @@FETCH_STATUS = 0
     BEGIN
-        -- If login exists, fix orphan
-        IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = @userid)
+        IF EXISTS (SELECT 1 FROM sys.server_principals WHERE name = @UserName)
         BEGIN
-            SET @sql = N'USE ' + QUOTENAME(@dbname) + N';
-                IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = N''' + REPLACE(@userid, '''', '''''') + N''')
-                BEGIN
-                    ALTER USER ' + QUOTENAME(@userid) + N' WITH LOGIN = ' + QUOTENAME(@userid) + N';
-                END
-            ';
-            IF @PrintOnly = 1
-                PRINT @sql;
-            ELSE
-                BEGIN TRY
-                    EXEC sp_executesql @sql;
-                    PRINT @sql;
-                END TRY
-                BEGIN CATCH
-                    PRINT 'Error: ' + ERROR_MESSAGE();
-                END CATCH
+            -- Relink User to Login
+            SET @DynamicSQL = N'USE ' + QUOTENAME(@CurrentDBName) + N'; ALTER USER ' + QUOTENAME(@UserName) + N' WITH LOGIN = ' + QUOTENAME(@UserName) + N';';
         END
         ELSE
         BEGIN
-            -- Drop user if no login exists, handle schema ownership
-            SET @sql = N'
-            USE ' + QUOTENAME(@dbname) + N';
-            DECLARE @schema NVARCHAR(128);
-            SELECT @schema = name FROM sys.schemas WHERE principal_id = USER_ID(N''' + REPLACE(@userid, '''', '''''') + N''');
-            IF @schema IS NOT NULL
-            BEGIN
-                DECLARE @auth_sql NVARCHAR(200);
-                SET @auth_sql = N''ALTER AUTHORIZATION ON SCHEMA '' + QUOTENAME(@schema) + N'' TO dbo;'';
-                EXEC sp_executesql @auth_sql;
-            END
-            IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = N''' + REPLACE(@userid, '''', '''''') + N''')
-            BEGIN
-                DROP USER ' + QUOTENAME(@userid) + N';
-            END
-            ';
-            IF @PrintOnly = 1
-                PRINT @sql;
-            ELSE
-                BEGIN TRY
-                    EXEC sp_executesql @sql;
-                    PRINT @sql;
-                END TRY
-                BEGIN CATCH
-                    PRINT 'Error: ' + ERROR_MESSAGE();
-                END CATCH
+            -- Fix Ownership then Drop
+            SET @DynamicSQL = N'USE ' + QUOTENAME(@CurrentDBName) + N'; 
+                DECLARE @schemaName NVARCHAR(128);
+                DECLARE @dropSQL NVARCHAR(MAX);
+                SELECT @schemaName = name FROM sys.schemas WHERE principal_id = USER_ID(' + QUOTENAME(@UserName, '''') + N');
+                IF @schemaName IS NOT NULL
+                BEGIN
+                    SET @dropSQL = N''ALTER AUTHORIZATION ON SCHEMA::'' + QUOTENAME(@schemaName) + N'' TO [dbo];'';
+                    EXEC sp_executesql @dropSQL;
+                END
+                SET @dropSQL = N''DROP USER '' + QUOTENAME(' + QUOTENAME(@UserName, '''') + N' ) + N'';'';
+                EXEC sp_executesql @dropSQL;';
         END
 
-        FETCH NEXT FROM user_cursor INTO @userid, @dbname;
+        IF @Print = 1
+        BEGIN
+            PRINT '-- Database: ' + @CurrentDBName + ' | User: ' + @UserName;
+            PRINT @DynamicSQL;
+        END
+
+        IF @Execute = 1
+        BEGIN
+            BEGIN TRY
+                EXEC sp_executesql @DynamicSQL;
+            END TRY
+            BEGIN CATCH
+                SET @ErrorMessage = 'Error in DB ' + @CurrentDBName + ' for User ' + @UserName + ': ' + ERROR_MESSAGE();
+                PRINT @ErrorMessage;
+            END CATCH
+        END
+
+        FETCH NEXT FROM user_cursor INTO @UserName, @CurrentDBName;
     END
 
     CLOSE user_cursor;
     DEALLOCATE user_cursor;
-
     DROP TABLE #OrphanUsers;
 END
 GO
